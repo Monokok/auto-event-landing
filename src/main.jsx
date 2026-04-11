023784a15c6d9337527b21f4775e8aed5f2ba08b
Monokok <monokok321@gmail.com>
2026-04-12 00:47:52 +0300

    chore: fix BrowserRouter basename in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/auto-event-landing/">
       <App />
     </BrowserRouter>
   </StrictMode>,
